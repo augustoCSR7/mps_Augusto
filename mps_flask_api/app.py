ed737a8303226e29904ec38e0e6ddc0b1530436a
Augusto César <acsds.snf21@uea.edu.br>
2023-03-16 16:22:28 -0400

    update code

--- mps_flask_api/app.py
+++ mps_flask_api/app.py
@@ -1,4 +1,4 @@
-from flask import render_template  # Remove: import Flask
+from flask import render_template # Remove: import Flask
 import connexion
 
 app = connexion.App(__name__, specification_dir="./")
@@ -8,7 +8,6 @@ app.add_api("swagger.yml")
 @app.route("/")
 def home():
     return render_template("home.html")
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
